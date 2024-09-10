a2a2e4392f4c2f6f0900e39b2174354ea81587b9
Cormac Farrelly <cfarr311y@gmail.com>
2024-09-11 00:11:37 +0100

    changing polling rate

--- js/updates.js
+++ js/updates.js
@@ -37,5 +37,5 @@ document.addEventListener('DOMContentLoaded', async () => {
     // Poll for updates every 5 seconds (5000 ms)
     setInterval(async () => {
         await fetchAndDisplayRepos();
-    }, 5000); // 5000 ms = 5 seconds
+    }, 10000); // 5000 ms = 5 seconds
 });
