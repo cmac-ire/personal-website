01e5a79f33f19c8e2b476421be4e6e908b4addc0
Cormac Farrelly <cfarr311y@gmail.com>
2024-09-12 17:37:08 +0100

    changes

--- health-check.js
+++ health-check.js
@@ -1,4 +1,3 @@
-// Function to perform the health check
 async function checkServerHealth() {
     try {
         const response = await fetch('https://bittersweet-bald-haircut.glitch.me/health');
@@ -13,8 +12,6 @@ async function checkServerHealth() {
     }
 }
 
-// Run the health check every 5 minutes (300000 milliseconds)
-setInterval(checkServerHealth, 300000);
+setInterval(checkServerHealth, 10000);
 
-// Run an initial check when the script loads
 checkServerHealth();
